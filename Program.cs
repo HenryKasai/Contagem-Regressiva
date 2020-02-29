280251ab88e672e9309fd556a7003f244f0b3d7f
HenryKasai <henrykasai1775@gmail.com>
2020-02-28 22:15:27 -0300

    Solução de exercícios

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Contagem_Regressiva
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine("Preparar...");
+            Console.ReadKey();
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("Apontar...");
+            Console.ReadKey();
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Fuego...");
+            Console.ReadKey();
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("BBBBBBBBBBBBBBBBB   ");
+            Console.WriteLine("B::::::::::::::::B       ");
+            Console.WriteLine("B::::::BBBBBB:::::B  ");
+            Console.WriteLine("BB:::::B     B:::::B                                          ");
+            Console.WriteLine(" B::::B     B:::::Buuuuuu    uuuuuu     mmmmmmm    mmmmmmm  ");
+            Console.WriteLine("  B::::B     B:::::Bu::::u    u::::u   mm:::::::m  m:::::::mm ");
+            Console.WriteLine("  B::::BBBBBB:::::B u::::u    u::::u  m::::::::::mm::::::::::m");
+            Console.WriteLine(" B:::::::::::::BB  u::::u    u::::u  m::::::::::::::::::::::m");
+            Console.WriteLine("  B::::BBBBBB:::::B u::::u    u::::u  m:::::mmm::::::mmm:::::m");
+            Console.WriteLine("  B::::B     B:::::Bu::::u    u::::u  m::::m   m::::m   m::::m");
+            Console.WriteLine("  B::::B     B:::::Bu::::u    u::::u  m::::m   m::::m   m::::m");
+            Console.WriteLine(" B::::B     B:::::Bu:::::uuuu:::::u  m::::m   m::::m   m::::m");
+            Console.WriteLine("BB:::::BBBBBB::::::Bu:::::::::::::::uum::::m   m::::m   m::::m");
+            Console.WriteLine("B:::::::::::::::::B  u:::::::::::::::um::::m   m::::m   m::::m");
+            Console.WriteLine("B::::::::::::::::B    uu::::::::uu:::um::::m   m::::m   m::::m");
+            Console.WriteLine("BBBBBBBBBBBBBBBBB       uuuuuuuu  uuuummmmmm   mmmmmm   mmmmmm");
+            Console.ReadKey();
+            Console.ForegroundColor = ConsoleColor.Gray;
+
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # Contagem-Regressiva
 Programa que faz uma contagem regressiva manual
+
+Solução do exercício:
+
+Faça um programa que exiba uma contagem regressiva no seguinte formato:
+
+Preparar...
+Apontar...
+FOGO!
+Aguarde o pressionamento de uma tecla antes de cada transição.
